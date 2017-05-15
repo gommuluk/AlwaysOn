3b0a48406e76ba47d5b23c9b31682f0f980a9441
hyomNam <skagbghk@gmail.com>
2017-05-15 20:24:39 +0900

    edit OnCreate

--- app/src/main/java/com/example/alwayson2017/alwayson2017/MainActivity.java
+++ app/src/main/java/com/example/alwayson2017/alwayson2017/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.alwayson2017.alwayson2017;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+    //사실 버튼말고 다른거 써야하는데 일단 프로토타입 내라고 해서 버튼으로 구성했음여
+    Button todaypick_space;
+    Button history_space;
+    Button camera_space;
+    Button store_space;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        todaypick_space = (Button) findViewById(R.id.todaypick_space);
+        history_space = (Button) findViewById(R.id.history_space);
+        camera_space = (Button) findViewById(R.id.camera_space);
+        store_space = (Button) findViewById(R.id.store_space);
+
+        todaypick_space.setOnClickListener(this);
+        history_space.setOnClickListener(this);
+        camera_space.setOnClickListener(this);
+        store_space.setOnClickListener(this);
     }
 }
